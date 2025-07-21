a1bed014833689b07aac1e88e34896285198f8ac
Majid G.E <mgetech@yahoo.com>
2025-07-22 00:23:27 +0200

    feat(docker): Complete dockerization with refined configurations

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8080/api/:path*',
+        destination: process.env.NEXT_PUBLIC_BACKEND_URL + '/api/:path*',
       },
     ];
   },
